98ea988b30fbb8431f491e828ebf927b68c8fe39
rishabhsvats <rishabhsvats@gmail.com>
2022-03-06 13:17:51 +0530

    Common Interface added. closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -4,14 +4,6 @@ import guru.springframework.sfgpetclinic.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
-
-    Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
+public interface OwnerService extends  CrudService<Owner,Long> {
 
 }
