70df5f902740e2c4e69e362167167baeca53296b
rishabhsvats <rishabhsvats@gmail.com>
2022-04-03 17:45:46 +0530

    Error. #62

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -17,8 +17,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -77,5 +78,13 @@ class OwnerControllerTest {
 
         Mockito.verifyNoInteractions(ownerService);
 
+    }
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+            mockMvc.perform(MockMvcRequestBuilders.get("/owners/123")).andExpect(status().isOk())
+                    .andExpect(view().name("owners/ownerDetails"))
+                    .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
     }
 }
